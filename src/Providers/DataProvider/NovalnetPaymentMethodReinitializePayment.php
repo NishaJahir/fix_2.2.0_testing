ae8572cbbfa34694466c1682c3472cc91089902b
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2021-09-29 09:28:21 +0530

    Update NovalnetPaymentMethodReinitializePayment.php

--- src/Providers/DataProvider/NovalnetPaymentMethodReinitializePayment.php
+++ src/Providers/DataProvider/NovalnetPaymentMethodReinitializePayment.php
@@ -22,14 +22,63 @@ use Plenty\Plugin\ConfigRepository;
 use Plenty\Modules\Basket\Contracts\BasketRepositoryContract;
 use Plenty\Modules\Frontend\Session\Storage\Contracts\FrontendSessionStorageFactoryContract;
 use Plenty\Modules\Payment\Contracts\PaymentRepositoryContract;
-use Plenty\Modules\Account\Address\Models\Address;
-use Plenty\Modules\Account\Address\Contracts\AddressRepositoryContract;
+
 
 class NovalnetPaymentMethodReinitializePayment
 {
   
   public function call(Twig $twig, $arg):string
   {
-     return '';
+     $order = $arg[0];
+    $paymentHelper = pluginApp(PaymentHelper::class);
+    $paymentService = pluginApp(PaymentService::class);
+    $config = pluginApp(ConfigRepository::class);
+    $basketRepository = pluginApp(BasketRepositoryContract::class);
+    $paymentRepository = pluginApp(PaymentRepositoryContract::class);
+    $sessionStorage = pluginApp(FrontendSessionStorageFactoryContract::class);
+    $payments = $paymentRepository->getPaymentsByOrderId($order['id']);
+    
+    
+    $paymentHelper->logger('address details', $address);
+    // Get payment method Id and status
+    foreach($order['properties'] as $property) {
+        if($property['typeId'] == 3)
+        {
+            $mopId = $property['value'];
+        }
+    }
+    
+     // Get transaction status
+    foreach($payments as $payment)
+    {
+        $properties = $payment->properties;
+        foreach($properties as $property)
+        {
+          if ($property->typeId == 30)
+          {
+          $tid_status = $property->value;
+          }
+        }
+    }
+    
+    
+      
+      // Changed payment method key
+       $paymentKey = $paymentHelper->getPaymentKeyByMop($mopId);
+    
+       
+       // If the Novalnet payments are rejected, do the reinitialize payment
+       if( !in_array($tid_status, [75, 85, 86, 90, 91, 98, 99, 100]) ) {
+         $paymentHelper->logger('tid status if 123', $tid_status);
+          return $twig->render('Novalnet::NovalnetPaymentMethodReinitializePayment', [
+            'order' => $order, 
+            'paymentMethodId' => $mopId,
+            'paymentKey' => $paymentKey,
+            
+          ]);
+       } else {
+         $paymentHelper->logger('tid status else 123', $tid_status);
+          return '';
+      }
   }
 }
